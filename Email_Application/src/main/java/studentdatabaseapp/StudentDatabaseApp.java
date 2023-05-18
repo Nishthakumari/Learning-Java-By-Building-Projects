00bb4d6afbaf8b48429fc21fa25e4346550fa7b5
Nishthakumari <knishtha0@gmail.com>
2023-05-18 18:17:27 +0530

    Creating the Outline for Student Database Application

---
+++ Email_Application/src/main/java/studentdatabaseapp/StudentDatabaseApp.java
@@ -0,0 +1,10 @@
+package studentdatabaseapp;
+
+public class StudentDatabaseApp {
+    public static void main(String[] args) {
+        //ask how many students we want to add
+
+        //Create n numbers of new students
+
+    }
+}

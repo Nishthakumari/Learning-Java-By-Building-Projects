e768f6193def4529309534d86ef6cd7c760eb89f
Nishthakumari <knishtha0@gmail.com>
2023-05-18 19:41:46 +0530

    Creating an Array of Objects

--- Email_Application/src/main/java/studentdatabaseapp/StudentDatabaseApp.java
+++ Email_Application/src/main/java/studentdatabaseapp/StudentDatabaseApp.java
@@ -1,15 +1,28 @@
 package studentdatabaseapp;
 
+import java.util.Scanner;
+
 public class StudentDatabaseApp {
     public static void main(String[] args) {
         //ask how many students we want to add
+        System.out.println("Enter number of courses to enroll");
+        Scanner sc = new Scanner(System.in);
+        int numOfStudent = sc.nextInt();
 
-        //Create n numbers of new students
+        Student[] students = new Student[numOfStudent];
 
-        Student st1 = new Student();
-        st1.enroll();
-        st1.payTuition();
-        System.out.println(st1.toString());
+        for(int n=0;n<numOfStudent;n++)
+        {
+            students[n] = new Student();
+            students[n].enroll();
+            students[n].payTuition();
+
+        }
+
+       for (int n=0;n<numOfStudent;n++)
+       {
+           System.out.println(students[n]);
+       }
 
     }
 }
